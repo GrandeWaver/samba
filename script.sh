16df5ed5a8d6dfea3f0dcd35448194b750afa4fd
Kacper Machaj <68419080+GrandeWaver@users.noreply.github.com>
2023-03-20 09:35:22 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade --yes
+sudo apt-get upgrade -yes
 sudo apt install samba
 systemctl status smbd --no-pager -l
 sudo systemctl enable --now smbd
